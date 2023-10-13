b702ee06732826c18e346b0f435306ffc55e61d9
amezianechayer <amezianechayer@github.com>
2023-10-14 00:43:41 +0200

    Adde d parse-json for ts

--- x/pages/index.tsx
+++ x/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement } from 'react';
+import React from 'react';
 
 export default function Home(): ReactElement {
-    return <h1>Hello, World</h1>;
-  }
-  
+  return <h1>Hello, World</h1>;
+}
